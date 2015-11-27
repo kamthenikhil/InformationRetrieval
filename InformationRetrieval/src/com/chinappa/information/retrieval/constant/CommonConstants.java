3d3d2e235155f69ad965ed813a8d056d5557ef93
Nikhil Kamthe <kamthenikhil@gmail.com>
2015-11-26 22:10:31 -0800

    Creating common project for information retrieval.

---
+++ InformationRetrieval/src/com/chinappa/information/retrieval/constant/CommonConstants.java
@@ -0,0 +1,14 @@
+package com.chinappa.information.retrieval.constant;
+
+public class CommonConstants {
+
+	public static final String UNDERSCORE = "_";
+	public static final String EMPTY_STRING = "";
+	public static final String HASH = "#";
+	public static final String SPACE = " ";
+	public static final String FULL_STOP = ".";
+	public static final String COLON = ":";
+	public static final String ASTERISK = "*";
+	public static final String FORWARD_SLASH = "/";
+	public static final String NEW_LINE = "\n";
+}
---
+++ InformationRetrieval/src/com/chinappa/information/retrieval/constant/SearchEngineConstants.java
@@ -0,0 +1,17 @@
+package com.chinappa.information.retrieval.constant;
+
+public class SearchEngineConstants {
+	
+	public static final String TITLE_FIELD = "title";
+
+	public static final String CONTENT_FIELD = "content";
+
+	public static final String METADATA_FIELD = "metadata";
+	
+	public static final String INDEX_FIELD = "index";
+	
+	public static final String INDEX_DIRECTORY = "index";
+	
+	public static final String CONFIGURATION_FILENAME = "root";
+
+}
